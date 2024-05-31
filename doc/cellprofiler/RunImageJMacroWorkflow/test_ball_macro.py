aa8e4ad89bfd52ab411da913287e52607fc8abf1
ramyses-rodrigues <ramyses.rmr@gmail.com>
2024-05-31 11:34:21 -0300

    up

--- doc/cellprofiler/RunImageJMacroWorkflow/test_ball_macro.py
+++
@@ -1,8 +0,0 @@
-#@String directory
-
-from ij import IJ
-import os
-
-im = IJ.open(os.path.join(directory, 'dummy.tiff'))
-IJ.run(im, "Subtract Background...", "rolling=50")
-IJ.saveAs(im,'tiff',os.path.join(directory,'backsub.tiff'))
--- doc/integration-use-cases.rst
+++
@@ -1,9 +0,0 @@
-Integration use cases
-======================
-
-These are use cases where PyImageJ has been integrated into other platforms/projects.
-
-.. toctree::
-    :maxdepth: 2
-
-    CellProfiler <cellprofiler/README>
